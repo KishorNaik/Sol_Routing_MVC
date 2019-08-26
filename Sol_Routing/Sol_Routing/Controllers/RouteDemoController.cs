9802d0b98d4e4a2c190d9f965db24b0ed61243f2
Kishor Naik <kishor.naik011.net@gmail.com>
2019-08-26 12:52:11 +0530

    Code Commit

---
+++ Sol_Routing/Sol_Routing/Controllers/RouteDemoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sol_Routing.Models;
+
+namespace Sol_Routing.Controllers
+{
+    [Route("web/route")]
+    public class RouteDemoController : Controller
+    {
+
+        //[Route("index")]
+        [HttpGet("index",Name ="index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [BindProperty(SupportsGet =true)]
+        public int id { get; set; }
+
+        [HttpGet("getvalue/{id:int}", Name ="getvalue")]
+        public IActionResult OnHttpGetValues()
+        {
+            return RedirectToRoute("index");
+        }
+
+        [BindProperty()]
+        public UserModel Users { get; set; }
+
+        [HttpPost("/post/onsubmit",Name ="onsubmit")]
+        public IActionResult OnHttpPostDemo()
+        {
+            return View("index");
+        }
+
+        [HttpPost("/post/onsubmitroutevalue",Name ="onsubmitroute")]
+        public IActionResult OnHttpDemo()
+        {
+            return View("index");
+        }
+       
+    }
+}
